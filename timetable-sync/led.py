2824de34117b8e26f059708acb5c4753dabe65e1
Kayden Lee <kayden@programmer.net>
2022-06-24 13:07:07 +0800

    update

--- timetable-sync/led.py
+++ timetable-sync/led.py
@@ -4,10 +4,16 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(True)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
 
 
 GPIO.output(18, GPIO.HIGH)
+GPIO.output(23, GPIO.HIGH)
+GPIO.output(24, GPIO.HIGH)
 print("on")
 time.sleep(5)
 GPIO.output(18, GPIO.LOW)
+GPIO.output(23, GPIO.LOW)
+GPIO.output(24, GPIO.LOW)
 print("off")
